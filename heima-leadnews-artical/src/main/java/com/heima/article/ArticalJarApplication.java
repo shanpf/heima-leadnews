7bf2900ca4c13293243c52e5f54f34173f402f40
单培峰 <shanpf@yonyou.com>
2020-10-09 20:39:33 +0800

    1.增加配置

---
+++ heima-leadnews-artical/src/main/java/com/heima/article/ArticalJarApplication.java
@@ -0,0 +1,12 @@
+package com.heima.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticalJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticalJarApplication.class,args);
+    }
+}
