741671f3fbffe9652efa903500c3915c4d9d71b7
单培峰 <shanpf@yonyou.com>
2020-10-09 20:18:12 +0800

    1.修改JDBC连接池信息 2.创建artical微服务

--- heima-leadnews-common/src/main/java/com/heima/common/common/storage/StorageEntry.java
+++ heima-leadnews-common/src/main/java/com/heima/common/common/storage/StorageEntry.java
@@ -1,12 +1,8 @@
 package com.heima.common.common.storage;
 
-<<<<<<< HEAD
 import lombok.Getter;
-=======
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
->>>>>>> 项目修改提交
 import lombok.Setter;
 
 /**
@@ -15,30 +11,9 @@ import lombok.Setter;
  */
 @Setter
 @Getter
-<<<<<<< HEAD
-public class StorageEntry {
-    /**
-     * 空的构造方法
-     */
-    public StorageEntry() {
-    }
-
-    /**
-     * 构造方法
-     *
-     * @param key
-     * @param value
-     */
-    public StorageEntry(String key, String value) {
-        this.key = key;
-        this.value = value;
-    }
-=======
 @NoArgsConstructor
 @AllArgsConstructor
 public class StorageEntry {
->>>>>>> 项目修改提交
-
     /**
      * 字段的Key
      */
